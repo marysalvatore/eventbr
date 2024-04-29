466fc6d43ff6f3cad433ab037ae4150f409df8d3
Mary Salvatore <marysalvatore084@gmail.com>
2024-04-29 13:42:44 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,104 @@
 import Image from "next/image";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebookF } from "react-icons/fa6";
+import { GrApple } from "react-icons/gr";
+import Footer from '../components/footer'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div>
+      <div className="flex flex-row bg-white">
+    <div className="flex sm:mt-[10em] w-[100%] md:mt-[-10em] lg:mt-[-12em] flex-col justify-center px-6 py-12 lg:px-8" >
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm" >
+        <svg style={{fill: '#f05537', width: '8em'}} viewBox="0 0 200 36">
+          <g fill-rule="evenodd">
+            <g>
+              <g transform="translate(.347)">
+                <path d="M185.945 17.513c2.693-.61 5.381.495 6.878 2.584l-11.905 2.693c.411-2.52 2.333-4.668 5.027-5.277zm6.944 9.91a6.57 6.57 0 01-3.979 2.679c-2.711.614-5.417-.51-6.908-2.626l11.942-2.702 1.945-.44 3.719-.841a11.782 11.782 0 00-.31-2.372c-1.513-6.426-8.055-10.432-14.611-8.949-6.556 1.484-10.644 7.896-9.13 14.321 1.513 6.426 8.055 10.433 14.61 8.95 3.864-.875 6.869-3.46 8.377-6.751l-5.655-1.269z"></path><path id="logo-wordmark-brand_svg__Fill-10" d="M164.788 35.118V18.082h-3.677v-5.804h3.677V4.289h6.244v7.989h4.69v5.804h-4.69v17.036z"></path><path d="M152.86 35.118h6.03v-22.84h-6.03v22.84zm-.785-30.853c0-2.114 1.667-3.7 3.825-3.7 2.157 0 3.775 1.586 3.775 3.7 0 2.115-1.618 3.748-3.775 3.748-2.158 0-3.825-1.633-3.825-3.748zM150.76 12.342c-3.082.16-4.9.633-6.75 1.973v-2.037h-6.026v22.84h6.026v-11.2c0-3.524.86-5.529 6.75-5.726v-5.85zM117.16 24.057c.15 3.333 3.051 6.128 6.602 6.128 3.601 0 6.552-2.942 6.552-6.422 0-3.432-2.95-6.373-6.552-6.373-3.551 0-6.452 2.843-6.602 6.128v.539zm-5.88 11.061V1.38l6.03-1.364v13.962c1.863-1.49 4.07-2.115 6.472-2.115 6.864 0 12.355 5.286 12.355 11.918 0 6.583-5.491 11.965-12.355 11.965-2.403 0-4.609-.624-6.472-2.114v1.487h-6.03z"></path><path id="logo-wordmark-brand_svg__Fill-1" d="M98.445 35.118V17.965h-3.677v-5.687h3.677V4.283l6.244-1.413v9.408h4.69v5.687h-4.69v17.153z"></path><path d="M87.394 35.118V22.915c0-4.421-2.402-5.382-4.805-5.382-2.402 0-4.805.913-4.805 5.286v12.299h-6.03v-22.84h6.03v1.699c1.324-.961 2.942-2.115 6.13-2.115 5.098 0 9.51 2.932 9.51 10.092v13.164h-6.03zM56.484 17.513c2.694-.61 5.382.495 6.878 2.584L51.458 22.79c.41-2.52 2.332-4.668 5.026-5.277zm6.945 9.91a6.57 6.57 0 01-3.98 2.679c-2.711.614-5.416-.51-6.907-2.626l11.942-2.702 1.944-.44 3.72-.841a11.782 11.782 0 00-.31-2.372c-1.514-6.426-8.056-10.432-14.612-8.949-6.556 1.484-10.644 7.896-9.13 14.321 1.513 6.426 8.055 10.433 14.611 8.95 3.863-.875 6.868-3.46 8.376-6.751l-5.654-1.269z"></path><path id="logo-wordmark-brand_svg__Fill-2" d="M31.89 35.118l-9.364-22.84h6.57l5.932 15.49 5.982-15.49h6.57l-9.365 22.84z"></path><path d="M10.703 17.507c2.694-.61 5.382.495 6.878 2.584L5.677 22.785c.41-2.52 2.332-4.668 5.026-5.278zm6.945 9.91a6.57 6.57 0 01-3.98 2.68c-2.71.613-5.416-.51-6.907-2.626l11.942-2.702 1.945-.44 3.718-.842a11.782 11.782 0 00-.31-2.371c-1.513-6.426-8.055-10.433-14.61-8.95C2.888 13.65-1.2 20.063.314 26.489c1.514 6.426 8.055 10.432 14.611 8.949 3.864-.874 6.869-3.46 8.376-6.75l-5.654-1.27z">
+                </path>
+              </g>
+            </g>
+          </g>
+        </svg>
+      {/* <Image width={69} className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" /> */}
+      <h1 className="mt-10 text-5xl font-bold leading-9 tracking-tight text-gray-900">Log in</h1>
+    </div>
+
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form className="space-y-6" action="#" method="POST">
+        <div>
+          {/* <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label> */}
+          <div className="mt-2">
+            <input id="email" placeholder="Email Address" name="email" type="email" autoComplete="email" required className="block w-full p-5 h-[3.5em] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+            {/* <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label> */}
+            {/* <div className="text-sm">
+              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+            </div> */}
+          </div>
+          <div className="mt-[-1em]">
+            <input id="password" placeholder="password" name="password" type="password" autoComplete="current-password" required className="block w-full h-[3.5em] p-5 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" className="flex w-full justify-center h-10 rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">Log in</button>
+        </div>
+      </form>
+      <div className="outer-div mt-6">
+            <div className="inner">or</div>
+      </div>
+
+      <div className="mt-10">
+          <button  className="flex w-full justify-center h-10 rounded-md bg-white-600 px-3 py-1.5 text-sm leading-6 text-black shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600" style={{border: '1px solid lightgray'}}>
+            Email me a login link
+          </button>
+      </div>
+
+      <div className="mt-3">
+          <button  className="flex w-full justify-center h-10 rounded-md bg-white-600 px-3 py-1.5 text-sm leading-6 text-black shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600" style={{border: '1px solid lightgray'}}>
+           <FcGoogle style={{width: '2em', height: '3em'}} className="mt-[-7px]" />  <span className="ml-3">Sign in with Google</span>
+          </button>
+      </div>
+
+
+      <p className="mt-10 text-sm text-gray-500">
+        Other login methods
+      </p>
+      <div className="flex pt-5">
+
+            <FaFacebookF style={{background: '#1877F2', fill: 'white', height: '3em', width: '3em',padding: '10px', borderRadius: '50%'}} />
+            <GrApple className='ml-3' style={{background: 'black', fill: 'white', height: '3em', width: '3em', padding: '10px', borderRadius: '50%'}} />
+
+
+      </div>
+    </div>
+    </div>
+    <div className="hidden w-[50%] shrink-0 sm:flex sm:flex-col sm:items-end">
+    <div className="info mt-[-12em]">
+    </div>
+
+    </div>
+
+
+
+  </div>
+  <Footer />
+</div>
+
+
+  );
+}
+
+
+
+
+{/* <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -108,6 +204,4 @@ export default function Home() {
           </p>
         </a>
       </div>
-    </main>
-  );
-}
+</main> */}
